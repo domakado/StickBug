cece5f65da67160a859944b320ae4c5c52eb0a7f
domakado <jenny@prochnow.eu>
2020-10-10 15:00:03 +0200

    initial commit

--- src/TheLastBug.java
+++ src/TheLastBug.java
@@ -2,5 +2,6 @@ public class TheLastBug {
 
     public static void main(String[] args){
         System.out.println("Beetle, Cricket, Dragonfly");
+        System.out.println("Queen-Bee");
     }
 }
